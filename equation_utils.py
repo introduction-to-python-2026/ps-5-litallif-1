c79e7413e0ad4876a9980ce0cb643f8615f94a95
litallif <liflital452@gmail.com>
2025-12-09 17:26:48 +0200

    Update equation_utils.py

--- equation_utils.py
+++ equation_utils.py
@@ -15,7 +15,7 @@ ELEMENTS = [
     'Md', 'No', 'Lr', 'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds',
     'Rg', 'Cn', 'Uut', 'Uuq', 'Uup', 'Uuh', 'Uus', 'Uuo'
 ]
-
+ 
 def generate_equation_for_element(compounds, coefficients, element):
     """Generates a symbolic equation for the given element from compounds and coefficients.  
     Example: For H in reactants [{'H': 2}, {'O': 4, 'H': 1}], coefficients [a0, a1], returns 2*a0 + a1."""
